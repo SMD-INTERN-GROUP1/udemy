d9ba27fd27f7231e3fd3974fd8c5059166b30dea
Nguyen Dung <nguyenanhdung462@gmail.com>
2022-01-03 15:08:08 +0700

    base constructor

---
+++ routes/home.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controller/home.controller');
+
+
+router.use('/', homeController.getHomePage);
+
+module.exports = router;
