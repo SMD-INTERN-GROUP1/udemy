7428af1bbbbd012edb5da42784b352b585ee5df4
Nguyen Dung <nguyenanhdung462@gmail.com>
2022-01-03 16:07:00 +0700

    basesource

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controller/home.controller');
 
-
 router.use('/', homeController.getHomePage);
 
 module.exports = router;
