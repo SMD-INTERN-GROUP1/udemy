1f51f64e3f6348449ecb839759f48faf6b24f0f7
tuannd20 <nhanchunggiuadoi@gmail.com>
2022-01-06 16:09:25 +0700

    Update header

--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,10 @@ const Categories = require("../database/models/Categories");
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   const categories = await Categories.find();
+  console.log(
+    "🚀 ~ file: home.js ~ line 9 ~ router.get ~ categories",
+    categories
+  );
   res.render("index", { title: "Express", categories });
 });
 
