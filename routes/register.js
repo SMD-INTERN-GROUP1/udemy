64eaecb629db0796dd4bc0e91661cd23046ccf10
hanh18 <htmhanh19@gmail.com>
2022-01-07 00:37:54 +0700

    validation register form

--- routes/register.js
+++ routes/register.js
@@ -4,27 +4,70 @@ const bodyParser = require('body-parser');
 const UserModel = require('../models/Users');
 const router = express.Router();
 
+let errs = [];
+
 /* GET register page. */
 router.get('/', (req, res, next) => {
-  res.render('component/register', { title: 'Register page' });
+  res.render('component/register', { title: 'Register page' , errs: errs});
 });
 
 router.post('/', (req, res, next) => {
   let username = req.body.username;
   let email = req.body.email;
   let password = req.body.password;
+  let confirmPassword = req.body.confirmPassword;
+  console.log(req.body);
 
-  UserModel.create({
-    username: username,
-    email: email,
-    password: password,
-  })
-  .then(data => {
-    res.json('Tạo tài khoản thành công');
-  })
-  .catch(err => {
-    res.json('Tạo tài khoản thất bại');
-  })
+  if(username.length < 6)
+    errs.push('Username must be at least 6 characters!');
+  
+  if(password.length < 8)
+    errs.push('Password must be at least 8 characters!');
+
+  if(password !== confirmPassword)
+    errs.push('Password, no match!');
+
+  if(errs.length > 0){
+      res.render('component/register', {
+          title: 'Register page',
+          errs: errs
+      });
+  }
+
+  if(errs.length == 0) {
+    UserModel.findOne({
+      username: username
+    })
+    .then(data => {
+      if(data) {
+        errs.push('Username already exist!');
+        res.render('component/register', { title: 'Register page' , errs: errs});
+      }
+    });
+
+    UserModel.findOne({
+      email: email
+    })
+    .then(data => {
+      if(data) {
+        errs.push('Email already exist!');
+        res.render('component/register', { title: 'Register page' , errs: errs});
+      }
+    });
+
+    UserModel.create({
+      username: username,
+      email: email,
+      password: password,
+    })
+    .then(data => {
+      //redirect login form
+      res.json('Tạo tài khoản thành công');
+    })
+    .catch(err => {
+      res.json('Tạo tài khoản thất bại');
+    })
+  }
 });
 
 module.exports = router;
