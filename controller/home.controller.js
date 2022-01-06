760ce1713ee464312ee6581f9000bb15355fd45b
Juhu <63043889+NguyenAnhDung0201@users.noreply.github.com>
2022-01-07 04:13:00 +0700

    Update home.controller.js

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -4,7 +4,7 @@ function getHomePage(req, res, next) {
 
     Course.find({}, function(err, courses){
         if(!err) {
-            res.render('index', {title: "Group1 SMD Number one", courses});
+            res.render('index', {courses});
         }
         else {
            console.log('This is err: ', err);
