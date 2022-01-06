54f2eddc4b263dafad85fbe1604d86fe53c88096
hanh18 <htmhanh19@gmail.com>
2022-01-06 12:22:41 +0700

    save registered account

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const UserModel = require('../models/Users');
 const router = express.Router();
 
 /* GET register page. */
@@ -7,6 +9,22 @@ router.get('/', (req, res, next) => {
   res.render('component/register', { title: 'Register page' });
 });
 
+router.post('/', (req, res, next) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
 
+  UserModel.create({
+    username: username,
+    email: email,
+    password: password,
+  })
+  .then(data => {
+    res.json('Tạo tài khoản thành công');
+  })
+  .catch(err => {
+    res.json('Tạo tài khoản thất bại');
+  })
+});
 
 module.exports = router;
