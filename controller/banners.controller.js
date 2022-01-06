6b4372aa93be9ac6156b2aa60529273004706ed9
tinnguyen14 <tinshb@gmail.com>
2022-01-07 02:52:37 +0700

    cm lan 2

--- controller/banners.controller.js
+++
@@ -1,8 +0,0 @@
-/* const banners = require("../database/models/banners");
-function bannerdata(req, res, next) {
-    banners.find({})
-    .then(banner => res.render('index', {banner}))
-    .catch(next);
-   
-}
-module.exports = {bannerdata} */
