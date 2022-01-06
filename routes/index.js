fa08726d51157b3664f1e102f8ce7770b9b4f83b
Nguyen Dung <nguyenanhdung462@gmail.com>
2022-01-07 03:24:30 +0700

    top-courses ver 1.0.1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const homeRouter = require('./home');
-
+const courseDetailRoute = require('./courses_detail');
 
 function route(app) {
   //write URL here
-  
+  app.use('/course', courseDetailRoute);
   app.use('/', homeRouter);
 }
 
