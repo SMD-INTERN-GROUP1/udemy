d7edc6e3b1ee2b5c759088c4c26621530324034c
Juhu <63043889+NguyenAnhDung0201@users.noreply.github.com>
2022-01-10 17:32:18 +0700

    Update home.controller.js

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -2,7 +2,7 @@ const categoryService = require("../services/category.services");
 const Course = require('../database/models/Courses');
 
 const getHomePage = async (req, res, next) => {
-  const categories = await categoryService.getListCategories();
+  const categories = await categoryService.getListCategory();
   const courses = await Course.find();
   res.render("index", { title: "Udemy", categories, courses});
 };
