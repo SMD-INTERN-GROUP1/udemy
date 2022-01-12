8cad47ee90310340cefd14789e52ce003b075257
tuannd20 <nhanchunggiuadoi@gmail.com>
2022-01-12 13:54:15 +0700

    add an variable in .env

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
 const homeRouter = require("./home");
 const courseDetailRoute = require("./courses_detail");
 const adminRouter = require("./admin");
-<<<<<<< HEAD
+const registerRouter = require("./register");
 const trashRouter = require("./trash");
-=======
-const registerRouter = require('./register');
->>>>>>> 0c2cc0440d65468492548a630e4395a95792c524
 
 function route(app) {
   //write URL here
   app.use("/admin/trash", trashRouter);
   app.use("/course", courseDetailRoute);
   app.use("/admin", adminRouter);
-  app.use('/register', registerRouter);
+  app.use("/register", registerRouter);
   app.use("/", homeRouter);
 }
 
