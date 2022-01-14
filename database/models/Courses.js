95a594e801db08a646e2add1b5e8279a6ef4d884
tuannd20 <nhanchunggiuadoi@gmail.com>
2022-01-14 16:19:28 +0700

    Teacher role

--- database/models/Courses.js
+++ database/models/Courses.js
@@ -1,40 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const course = new mongoose.Schema({
-    image:{
-        type:String,
-        default:''
+const course = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      default: "",
     },
-    title:{
-        type:String,
-        default:''
+    title: {
+      type: String,
+      default: "",
     },
     author: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
-    description:{
-        type:String,
-        default:''
+    description: {
+      type: String,
+      default: "",
     },
-    start:{
-        type:Number,
-        default:0
+    price: {
+      type: Number,
+      default: 0,
     },
-    price:{
-        type:Number,
-        default:0
+    isDiscount: {
+      type: Boolean,
+      default: false,
     },
-    isDiscount:{
-        type:Boolean,
-        default:false
-    },
-    email:{
-        type:String,
-        default:''
+    topic_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Topic",
     },
     // kids: [{type:mongoose.Schema.Types.ObjectId}]
-},{timestamps:true}
+  },
+  { timestamps: true }
 );
 
-module.exports=mongoose.model("Courses",course);
+module.exports = mongoose.model("Courses", course);
---
+++ database/models/Topics.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+
+const topic = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    category_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Categories",
+    },
+  },
+  { timestamps: true }
+);
+
+// Add plugins
+topic.plugin(mongooseDelete, { deletedAt: true, overrideMethods: "all" });
+
+module.exports = mongoose.model("Topic", topic);
