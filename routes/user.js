ab84cbb9dc2e035cfbe50b5e11de6147d943edf7
hanh18 <htmhanh19@gmail.com>
2022-01-18 15:42:23 +0700

    done BE form change password

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const Users = require('../database/models/Users')
+const bcrypt = require("bcrypt");
+const Users = require('../database/models/Users');
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ router.get('/', (req, res, next) => {
   // });
 });
 
-router.get('/edit-profile', async (req, res, next) => {
+router.get('/edit-profile', (req, res, next) => {
   try {
     let isLogin = false;
   
@@ -31,7 +32,7 @@ router.get('/edit-profile', async (req, res, next) => {
   };
 });
 
-router.patch('/edit-profile', async (req, res, next) => {
+router.patch('/edit-profile', (req, res, next) => {
   try {
     let isLogin = false;
     let idUser;
@@ -49,13 +50,14 @@ router.patch('/edit-profile', async (req, res, next) => {
   }
 });
 
-router.get('/edit-account', async (req, res, next) => {
+router.get('/edit-account', (req, res, next) => {
   try {
-    if (req.cookies.user) {
+    let notification = "";
 
+    if (req.cookies.user) {
       Users.findOne({_id: req.cookies.user._id})
       .then(user => {
-        res.render("profile/account.ejs", { title: "User account", user } );
+        res.render("profile/account.ejs", { title: "User account", user, notification } );
       })
       .catch(next);
     } else {
@@ -64,6 +66,69 @@ router.get('/edit-account', async (req, res, next) => {
   } catch(err) {
     return res.status(500).json(err);
   };
+});
+
+router.patch('/edit-account', async (req, res, next) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedNewPass = await bcrypt.hash(req.body.newPassword, salt);
+    let newPass = hashedNewPass;
+
+    if (req.cookies.user) {
+      let idUser = req.cookies.user._id;
+      let notification;
+      let user = req.cookies.user;
+
+      // let query = { _id: idUser, password: currentPass}
+      // Users.findOneAndUpdate(query, {password: newPass})
+      // .then((data) => {
+      //   console.log('data user', data);
+      //   if(data) {
+      //     notification = "Password update successful";
+      //     res.render("profile/account.ejs", { title: "User account", data, notification } );
+      //     // res.json('đúng');
+      //   } else {
+      //     notification = "Incorrect password";
+      //     res.render("profile/account.ejs", { title: "User account", user, notification } );
+      //     // res.json('sai');
+      //   }
+      // })
+      // .catch(err => console.log(err));
+
+
+      const checkUser = await Users.findOne({email : req.cookies.user.email}); //find by ID
+      console.log(checkUser);
+      if(!checkUser){
+        // json('Sai tài khoản!');
+        return res.status(404).render('component/login');
+      }
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        checkUser.password
+      );
+      if(!validPassword){
+          // json('Sai mật khẩu!');
+          // return res.status(404).render('component/login');
+          // notification = "Incorrect password";
+          // res.json(notification);
+          // res.render("profile/account.ejs", { title: "User account", user, notification } );
+          notification = "Incorrect";
+          res.render("profile/account.ejs", { title: "User account", user, notification } );
+      } else {
+        Users.updateOne( { _id: idUser } , { password: newPass} )
+        .then(() => {
+          notification = "Successful";
+          res.render("profile/account.ejs", { title: "User account", user, notification } );
+        });
+      }
+
+      
+      // notification = "Correct password";
+      // res.json(notification);
+
+      // console.log(req.body);
+      // res.json(req.body);
+    }
+  
 });
 
 // router.get('/public-profile', async (req, res, next) => {
