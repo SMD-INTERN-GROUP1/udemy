862ea7dfdea071ad172cbfe79fd069bbb3d86ba6
tinnguyen14 <tinshb@gmail.com>
2022-01-19 16:48:37 +0700

    commit first

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ app.use(express.static(path.join(__dirname, "public")));
 route(app);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+// app.use((req, res, next) => {
+//   next(createError(404));
+// });
 
 // error handler
 app.use((err, req, res, next) => {
@@ -60,8 +60,8 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  // res.status(err.status || 500);
+  // res.render("error");
 });
 
 module.exports = app;
--- controller/course.controller.js
+++ controller/course.controller.js
@@ -1,5 +1,6 @@
 const Course = require("../database/models/course");
 const categoryService = require("../services/category.services");
+const User = require("../database/models/Users");
 
 const getDetailCourse = async (req, res, next) => {
   const slug = req.params.slug;
@@ -10,6 +11,7 @@ const getDetailCourse = async (req, res, next) => {
     isLogin = true;
     console.log("cookies", req.cookies.user);
   }
+
   res.render("component/course_detail", {
     course,
     categories,
@@ -23,7 +25,91 @@ const renderCoursePage = async (req, res, next) => {
     content: "../course/course_index",
   });
 };
+
+// create review
+const createReview = async (req, res) => {
+  const { comment, rate } = req.body;
+
+  const course = await Course.findById(req.params.id);
+  const user = await User.findOne({ username: req.cookies.user });
+
+  if (!course) {
+    return res.status(400).json({ err: "course is not exist !" });
+  }
+
+  const newReview = {
+    comment,
+    rate,
+    name: user.username,
+    user: user._id,
+    createdAt: Date.now(),
+  };
+
+  // course.reviews = [];
+  // await course.save();
+
+  course.reviews.push(newReview);
+
+  course.numberReview = course.reviews.length;
+
+  course.rating =
+    course.reviews.reduce((total, review) => {
+      return total + review.rate;
+    }, 0) / course.reviews.length;
+
+  await course.save();
+
+  return res.redirect("back");
+};
+
+const deleteReview = async (req, res) => {
+  const course = await Course.findById(req.params.idCourse);
+  const user = await User.findOne({ username: req.cookies.user });
+
+  if (!course) {
+    return res.status(400).json({ err: "course is not exist !" });
+  }
+
+  // await course.updateOne({
+  //   $pull: {
+  //     reviews: req.params.id,
+  //   },
+  // });
+
+  const index = course.reviews.findIndex((review) => {
+    return review._id == req.params.id;
+  });
+  if (index !== -1) {
+    if (
+      JSON.stringify(user._id) != JSON.stringify(course.reviews[index].user)
+    ) {
+      return res
+        .status(400)
+        .json({ err: "You can not delete review of the other people !" });
+    }
+    course.reviews.splice(index, 1);
+  }
+
+  if (course.reviews.length  > 0) {
+    course.numberReview = course.reviews.length;
+
+    course.rating =
+      course.reviews.reduce((total, review) => {
+        return total + Number(review.rate);
+      }, 0) / course.reviews.length;
+  } else {
+    course.numberReview = 0;
+
+    course.rating = 0;
+  }
+
+  await course.save();
+  return res.redirect("back");
+};
+
 module.exports = {
   getDetailCourse,
   renderCoursePage,
+  createReview,
+  deleteReview,
 };
