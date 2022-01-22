77e20597ccfeaaf9fc8fc9ae464bad4684fd8fed
tuannd20 <nhanchunggiuadoi@gmail.com>
2022-01-22 17:34:36 +0700

    update button add quizz

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,7 +2,8 @@ const UsersModel = require("../database/models/Users");
 const Cart = require("../database/models/Cart");
 const Course = require("../database/models/Courses");
 const Chapter = require("../database/models/Chapters");
-const Proccess = require("../database/models/Proccess")
+const Proccess = require("../database/models/Proccess");
+const CategoriesModel = require("../database/models/Categories");
 
 const renderUserPage = async (req, res, next) => {
   const users = await UsersModel.find();
@@ -14,8 +15,9 @@ const renderUserPage = async (req, res, next) => {
 };
 
 //my learning
-const getMyLearning = async(req, res, next) => {
+const getMyLearning = async (req, res, next) => {
   try {
+    const categories = await categoryService.getListCategory();
     //get user id
     //find course by user id
     let isLogin = true;
@@ -26,80 +28,73 @@ const getMyLearning = async(req, res, next) => {
       user = req.cookies.user;
     }
     let user_id = "61d696d0896327cb23460f8c";
-    let cart = await Cart.findOne({user_id: user_id});
+    let cart = await Cart.findOne({ user_id: user_id });
     let list_course = [];
-    for(let i = 0; i < cart.listCarts.length; i++) {
+    for (let i = 0; i < cart.listCarts.length; i++) {
       let course = await Course.findById({ _id: cart.listCarts[i] });
-      if( course !== null ) {
+      if (course !== null) {
         list_course.push(course);
-      }        
+      }
     }
-    res.render("component/my-learning", {list_course});
-  } catch(error) {
-    console.log('err: ', error);
+    // res.render("component/my-learning", { list_course, categories });
+  } catch (error) {
+    console.log("err: ", error);
     res.json({ msg: error });
   }
 };
 
 //learning
-const getListVideoToLearn = async(req, res, next) => {
+const getListVideoToLearn = async (req, res, next) => {
   try {
     const course = await Course.findOne({ slug: req.params.slug });
-    const list_chapter = await Chapter.find({course_id: course._id});
-    console.log('This is list_chapter: ',list_chapter);
-    res.render('component/learning-course', {course, list_chapter});
-  } catch(error) {
-    res.status(500).json({ "msg": error });
+    const list_chapter = await Chapter.find({ course_id: course._id });
+    console.log("This is list_chapter: ", list_chapter);
+    res.render("component/learning-course", { course, list_chapter });
+  } catch (error) {
+    res.status(500).json({ msg: error });
   }
-}
+};
 
-const createProccess = async(req, res, next) => {
+const createProccess = async (req, res, next) => {
   try {
     let obj = req.body;
     obj.user_id = "61d696d0896327cb23460f8c";
     obj.course_id = "61e2d7cddf1e49b94d069a84";
     console.log(obj);
     let proccessObj = new Proccess(obj);
-    console.log('count: ',req.body.count);
-    if(req.body.count > 0) {
+    console.log("count: ", req.body.count);
+    if (req.body.count > 0) {
       proccessObj.save();
     }
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
-}
-const toggleWish = async (req, res,next) => {
+};
+
+const toggleWish = async (req, res, next) => {
   const idCourse = req.params.id;
-  const user = await UsersModel.findOne({username : req.user.username});
+  const user = await UsersModel.findOne({ username: req.user.username });
   const { wishList } = user;
   if (wishList.includes(idCourse)) {
-    const newUser = await UsersModel.findByIdAndUpdate(
-     user._id ,
-      {
-        $pull: {
-          wishList: idCourse,
-        },
+    const newUser = await UsersModel.findByIdAndUpdate(user._id, {
+      $pull: {
+        wishList: idCourse,
       },
-    );
+    });
     return res.redirect("back");
   } else {
-    const newUser = await UsersModel.findByIdAndUpdate(
-    user._id ,
-      {
-        $push: {
-          wishList: idCourse,
-        },
+    const newUser = await UsersModel.findByIdAndUpdate(user._id, {
+      $push: {
+        wishList: idCourse,
       },
-     
-    );
-   return res.redirect("back");
+    });
+    return res.redirect("back");
   }
 };
-module.exports = { 
+module.exports = {
   renderUserPage,
   getMyLearning,
   getListVideoToLearn,
   createProccess,
-  toggleWish
+  toggleWish,
 };
-
