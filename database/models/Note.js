32a254a2aeb01455c503cd7f67c7f509b1977cc4
unknown <luukimhoangdz2022@gmail.com>
2022-01-23 20:09:38 +0700

    update abc

---
+++ database/models/Note.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const note = new mongoose.Schema({
+  video_id:{
+    type:mongoose.Schema.Types.ObjectId
+  },
+  note_lists: [
+    {
+      type: mongoose.Schema.Types.ObjectId
+    },
+  ],
+},{timestamps:true}
+);
+
+module.exports = mongoose.model("Note", note);
