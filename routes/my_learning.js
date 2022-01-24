157e85c906f50f50de9f5bab5c51715203d04741
tuannd20 <nhanchunggiuadoi@gmail.com>
2022-01-24 16:39:09 +0700

    Update file controller

--- routes/my_learning.js
+++ routes/my_learning.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controller/user.controller');
+const userController = require("../controller/user.controller");
 const questionController = require("../controller/questionController");
 router.get("/", userController.getMyLearning);
 router.get("/:slug", userController.getListVideoToLearn);
 
-router.post("/check/:idCourse",  questionController.checkQuestion)
+router.post("/check/:idCourse", questionController.checkQuestion);
 router.get("/quiz/:slug", questionController.test);
-router.post("/process/store", userController.createProccess);
-
+router.post("/process/store", userController.createProgress);
 
 module.exports = router;
