b5b4abdafadc21c800899ef5f7f4da6c4b502d5e
tuannd20 <nhanchunggiuadoi@gmail.com>
2022-01-25 21:48:17 +0700

    Add count user in admin page Tuan

--- controller/admin.controler.js
+++ controller/admin.controler.js
@@ -1,8 +1,24 @@
-const renderAdminPage = (req, res, next) => {
-  res.render("dashboard_admin/master", {
-    title: "Admin page",
-    content: "../dashboard_admin/main_content",
-  });
+const CourseModel = require("../database/models/Courses");
+const UserModel = require("../database/models/Users");
+
+const renderAdminPage = async (req, res, next) => {
+  try {
+    const countUsers = await UserModel.find({
+      isTeacher: false,
+    }).countDocuments();
+    console.log(
+      "🚀 ~ file: admin.controler.js ~ line 7 ~ renderAdminPage ~ countUsers",
+      countUsers
+    );
+
+    res.render("dashboard_admin/master", {
+      title: "Admin page",
+      content: "../dashboard_admin/main_content",
+      countUsers,
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 module.exports = renderAdminPage;
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const CourseModel = require("../database/models/Courses");
+const UserModel = require("../database/models/Users");
+const TopicModel = require("../database/models/Topics");
+
 const adminController = require("../controller/admin.controler");
 const categoriesController = require("../controller/category.controller");
 const topicController = require("../controller/topic.controller");
@@ -9,11 +13,28 @@ const userController = require("../controller/user.controller");
 const bannerController = require("../controller/banner.controller");
 
 /* GET admin page. */
-router.get("/", (req, res, next) => {
-  res.render("dashboard_admin/master", {
-    title: "Admin page",
-    content: "../dashboard_admin/main_content",
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    const countStudents = await UserModel.find({
+      isTeacher: false,
+    }).countDocuments();
+    const countUsers = await UserModel.find({}).countDocuments();
+    const countInstructor = await UserModel.find({
+      isTeacher: true,
+    }).countDocuments();
+    const countCourses = await CourseModel.find({}).countDocuments();
+
+    res.render("dashboard_admin/master", {
+      title: "Admin page",
+      content: "../dashboard_admin/main_content",
+      countStudents,
+      countInstructor,
+      countUsers,
+      countCourses,
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 // Users section
